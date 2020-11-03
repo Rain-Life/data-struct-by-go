ed1d37083ce0f62cb5ed95b4bbd8d57d1e032858
陈兴生 <chenxingsheng@51shebao.com>
2020-11-03 19:51:19 +0800

    插入排序

--- sort/insertSort/insertSort.go
+++ sort/insertSort/insertSort.go
@@ -1,2 +1,23 @@
 package insertSort
 
+import "fmt"
+
+func insertSort(arr []int)  {
+	if len(arr) <= 0 {
+		fmt.Println("待排数据不合法")
+	}
+	n := len(arr)
+
+	for i := 1; i < n; i++ {//i是待排区的元素
+		value := arr[i]
+		j := i-1
+		for ; j >= 0; j-- { //j遍历的是已排区的每一个元素
+			if arr[j] > value {
+				arr[j+1] = arr[j] //如果满足条件，将前一个值赋给后边这个
+			} else {
+				break
+			}
+		}
+		arr[j+1] = value
+	}
+}
