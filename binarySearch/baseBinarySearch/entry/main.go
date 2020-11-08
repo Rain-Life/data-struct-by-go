5c192bc010dea53324479921ef9d76d19ecab90c
陈兴生 <chenxingsheng@51shebao.com>
2020-11-08 14:22:45 +0800

    递归实现二分查找

--- binarySearch/baseBinarySearch/entry/main.go
+++ binarySearch/baseBinarySearch/entry/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	arr := []int{2,3,4,5,6,7,8,9}
-	position := baseBinarySearch.BinarySearch(arr, 9)
+	position := baseBinarySearch.BinarySearch(arr, 2)
 	fmt.Println(position)
+
+	position1 := baseBinarySearch.RecursionBinarySearch(arr, 0, len(arr)-1, 10)
+	fmt.Println(position1)
 }
