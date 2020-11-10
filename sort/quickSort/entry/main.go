6270d39235fa6561584ab3929968200353d9c56b
陈兴生 <chenxingsheng@51shebao.com>
2020-11-10 17:40:56 +0800

    快速排序

--- sort/quickSort/entry/main.go
+++ sort/quickSort/entry/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"data-struct-by-go/sort/quickSort"
+	"fmt"
+)
 
+func main() {
+	arr := []int{6, 4, 5, 1, 8, 7, 2, 3}
+	fmt.Println("排序之前")
+	fmt.Println(arr)
+	quickSort.QuickSort(arr, 0, len(arr)-1)
+	fmt.Println("排序之后")
+	fmt.Println(arr)
 }
--- sort/quickSort/quickSort.go
+++ sort/quickSort/quickSort.go
@@ -1,3 +1,28 @@
 package quickSort
 
+func QuickSort(arr []int, start int, end int) {
+	if start >= end {
+		return
+	}
+
+	position := partition(arr, start, end)
+	QuickSort(arr, start, position-1)
+	QuickSort(arr, position+1, end)
+}
+
+
+func partition(arr []int, start int, end int) int {
+	pivot := arr[end]
+	i := start
+	for j:=start;j < end;j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j],arr[i]
+			i++
+		}
+	}
+	arr[i], arr[end] = arr[end], arr[i]
+
+	return i
+}
+
 
