1543e1a59760a55783061a2dc15c4df598a4724d
陈兴生 <chenxingsheng@51shebao.com>
2020-11-14 15:33:14 +0800

    在 O(n) 的时间复杂度内查找一个无序数组中的第 K 大元素

--- sort/quickSort/entry/main.go
+++ sort/quickSort/entry/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	arr := []int{6, 4, 5, 1, 8, 7, 2, 3}
-	fmt.Println("排序之前")
-	fmt.Println(arr)
-	quickSort.QuickSort(arr, 0, len(arr)-1)
-	fmt.Println("排序之后")
-	fmt.Println(arr)
+	arr := []int{6,1,3,5,7,2,4,9,11,8}
+	//fmt.Println("排序之前")
+	//fmt.Println(arr)
+	//quickSort.QuickSort(arr, 0, len(arr)-1)
+	//fmt.Println("排序之后")
+	//fmt.Println(arr)
+
+	topK := quickSort.FindTopK(arr, 3)
+	fmt.Println("第三大的元素是")
+	fmt.Println(topK)
 }
--- sort/quickSort/quickSort.go
+++ sort/quickSort/quickSort.go
@@ -13,6 +13,7 @@ func QuickSort(arr []int, start int, end int) {
 
 func partition(arr []int, start int, end int) int {
 	pivotValue := arr[end]
+	//fmt.Println(pivotValue)
 	i := start
 	for j:=start;j < end;j++ {
 		if arr[j] < pivotValue {
@@ -25,4 +26,23 @@ func partition(arr []int, start int, end int) int {
 	return i
 }
 
+// 在 O(n) 的时间复杂度内查找一个无序数组中的第 K 大元素？
 
+func FindTopK(arr []int, K int) int {
+	return TopK(arr, 0, len(arr)-1, K)
+}
+
+func TopK(arr []int, start int, end int, K int) int {
+	//if start >= end { 这里是个坑，不能加这个限制条件
+	//	return -1
+	//}
+	pivot := partition(arr, start, end)
+
+	if pivot+1 == K {
+		return arr[pivot]
+	} else if pivot+1 < K {
+		return TopK(arr, pivot+1, end, K)
+	} else {
+		return TopK(arr, start, pivot-1, K)
+	}
+}
