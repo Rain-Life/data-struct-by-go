30c4fff37a67fea0defbcfc5d9320bd597f0a134
陈兴生 <chenxingsheng@51shebao.com>
2020-11-14 16:58:19 +0800

    求一个数的平方根,要求精确到小数点后 6 位

--- binarySearch/practice/practiceBinarySearch.go
+++ binarySearch/practice/practiceBinarySearch.go
@@ -103,3 +103,36 @@ func BinarySearchLastSmaller(arr []int, value int) int {
 
 	return -1
 }
+
+//“求一个数的平方根”？要求精确到小数点后 6 位
+func SolutionSqrt(num float64, precision float64) float64 {
+	if num < 0 {
+		fmt.Println("参数不合法")
+		return -1.000
+	}
+
+	var low,high float64
+	if num > 1 {
+		low = 1
+		high = num
+	} else {
+		low = num
+		high = 1
+	}
+
+	return Sqrt(num, low, high, precision)
+}
+
+func Sqrt(num float64, low float64, high float64, precision float64) int {
+	mid := (low+high) / 2
+
+	if num - (mid * mid) < 0 {
+		return Sqrt(num, low, (mid+high)/2, precision)
+	} else {
+		if num - (mid * mid) > precision {
+			return Sqrt(num, (low + mid)/2, high, precision)
+		}
+
+		return mid.(int)
+	}
+}
